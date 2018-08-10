116f7ef6868be3ac8759b8addcb82ddae0bbfa12
liahan <liahan@cisco.com>
2018-08-10 16:57:42 +0800

    sonarqube

--- bin/mstest.bat
+++ bin/mstest.bat
@@ -9,8 +9,6 @@ rmdir /s /q TestResults
 set PATH=%PATH%;%CD%;
 "%VS150COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" WebexSDKTests.dll /InIsolation /EnableCodeCoverage /Logger:trx /Settings:..\..\..\..\..\..\sdk\WebexSDKTests\CodeCoverage.runsettings 2>vstest_error_log.txt
 
-
-
 echo vstest.console.exe exit code is %errorlevel%
 set "abort_flag=the execution process exited unexpectedly"
 
@@ -28,6 +26,7 @@ CALL set "test=!!vstest_error:%abort_flag%=!!"
 echo !test!
 if "!test!"=="!vstest_error!" (
      ..\..\..\..\..\..\bin\CoverageConverter.exe /in:TestResults/*.coverage /out:TestResults/vstest.coveragexml
+	 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\CodeCoverage.exe" analyze /output:TestResults\VisualStudio.coveragexml TestResults\*.coverage"
 ) else ( 
      rmdir /s /q TestResults
      echo "don't calculate coverage because the test process exited unexpectly"
@@ -37,6 +36,7 @@ if "!test!"=="!vstest_error!" (
     echo %errorlevel%
 ) else (
 ..\..\..\..\..\..\bin\CoverageConverter.exe /in:TestResults/*.coverage /out:TestResults/vstest.coveragexml
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\CodeCoverage.exe" analyze /output:TestResults\VisualStudio.coveragexml TestResults\*.coverage"
 )
 echo %errorlevel%
 popd
